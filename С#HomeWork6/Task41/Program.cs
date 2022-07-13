0084ffc094c568a086d8489bb211d47429f2e8a1
Nikita Balenko <nikitabalenko2013@gmail.com>
2022-07-14 01:32:05 +0300

    Выполнил дз 6

---
+++ С#HomeWork6/Task41/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] createArray()
+{
+    Console.Write("Введите размер массива: ");
+    int size = int.Parse(Console.ReadLine());
+    int[] Result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        System.Console.Write($"Введите {i+1} элемент массива: ");
+        Result[i] = int.Parse(Console.ReadLine());
+    }
+    return Result;
+}
+
+int greatThenNull(int[] array)
+{
+    int Result = 0;
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] > 0)
+        {
+            Result++;
+        }
+    }
+    return Result;
+}
+
+
+int[] arr = createArray();
+System.Console.WriteLine($"Массив: {String.Join(", ", arr)}");
+System.Console.WriteLine($"Элементов массива больше чем 0: {greatThenNull(arr)}");
---
+++ С#HomeWork6/Task43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.Write("введите b1: ");
+int b1 = int.Parse(Console.ReadLine());
+System.Console.Write("введите k1: ");
+int k1 = int.Parse(Console.ReadLine());
+System.Console.Write("введите b2: ");
+int b2 = int.Parse(Console.ReadLine());
+System.Console.Write("введите k2: ");
+int k2 = int.Parse(Console.ReadLine());
+if (k1 == k2 || b1 == b2) System.Console.WriteLine("Нет точек песечения");
+else System.Console.WriteLine($"Координаты точки пересечеия: ({string.Join("; ", intersectionPoint())})");
+
+float[] intersectionPoint()
+{
+    float b = b2 - b1;
+    float k = k1 - k2;
+    float x = b / k;
+    float y = k1 * x + b1;
+    float[] result = new float [2] {x, y};
+    return result;
+}
